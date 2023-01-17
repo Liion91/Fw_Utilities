50d12d83aca01fc741c9a3b40674f0003e45e3c1
Leonardo <l.menghini@reply.it>
2023-01-17 16:19:38 +0100

    add redirect uyility

---
+++ productsBylineOnly/redirect.js
@@ -0,0 +1,12 @@
+const base = "url";
+let jsonFile = require("./prods_filtered_prod.json");
+let fs = require("fs");
+
+const jsonUrls = JSON.stringify(jsonFile.map((el) => base + el));
+fs.writeFile("myJsonfileProd.json", jsonUrls, "utf8", (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("JSON data is saved.");
+});
+
